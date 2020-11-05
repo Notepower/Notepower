cbd007451a80de7d054712c85dc030a02c226e75
Notepower <56997375+Notepower@users.noreply.github.com>
2020-11-05 15:54:02 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Daylamotproject2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            /*HoaDon hd = new HoaDon();
+            hd.Nhap();
+            hd.Xuat();
+            */
+            BAI3 gio = new BAI3();
+            gio.Nhap();
+            gio.Show24();
+            gio.Show12();
+            gio.CongPhut();
+        }
+    }
+}
