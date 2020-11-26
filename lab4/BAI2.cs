8dd010f25bfce99d47ab87e68ee15fb44791227f
Notepower <56997375+Notepower@users.noreply.github.com>
2020-11-26 14:50:41 +0700

    Add files via upload

---
+++ lab4/BAI2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace daylamotproject
+{
+    class Colour
+    {
+        protected int colour;
+        public Colour()
+        {
+            colour = 0;
+        }
+        public Colour(int c1)
+        {
+            colour = c1;
+        }
+    }
+    class Pixel : Colour
+    {
+        int x, y;
+        public Pixel()
+        {
+            x = 0; y = 0;
+        }
+        public Pixel(int x1, int y1, int c1) : base(c1)
+        {
+            x = x1; y = y1;
+        }
+        public void Set()
+        {
+            Console.Write("Nhập màu của pixel: ");
+            colour = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập tọa độ của pixel: ");
+            Console.Write("x = ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y = ");
+            y = Convert.ToInt32(Console.ReadLine());
+        }
+        public bool KTCheoChinh()
+        {
+            return x == y;
+        }   
+        public void InPixcel()
+        {
+            Console.ForegroundColor = (ConsoleColor)colour;
+            Console.CursorTop = y;
+            Console.CursorLeft = x;
+            Console.Write("o");
+        }
+    }
+}
---
+++ lab4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace daylamotproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pixel[] arrayP = new Pixel[100];
+            Console.Write("Nhập số pixel cần xử lý: ");
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            Console.WriteLine("\t\t\n\nNHẬP PIXEL");
+            for (int i = 0; i < n; i++)
+            {
+                arrayP[i] = new Pixel();
+                arrayP[i].Set();
+            }
+            Console.Clear();
+            for (int i = 0; i < n; i++)
+            {
+                arrayP[i].InPixcel();
+            }
+            Console.ReadKey();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\t\nCác pixel trên đường chéo chính: ");
+            for (int i = 0; i < n; i++)
+                if (arrayP[i].KTCheoChinh())
+                {
+                    arrayP[i].InPixcel();
+                    count++;
+                }
+            Console.WriteLine("Số pixel trên đường chéo chính là: " + count + " điểm.");
+        }
+    }
+}
