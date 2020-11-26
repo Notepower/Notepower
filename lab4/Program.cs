d3c5e6bac0088f8c768c54154df58c585654859a
Notepower <56997375+Notepower@users.noreply.github.com>
2020-11-26 14:56:07 +0700

    Update Program.cs

--- lab4/Program.cs
+++ lab4/Program.cs
@@ -6,11 +6,11 @@ namespace daylamotproject
     {
         static void Main(string[] args)
         {
-            Pixel[] arrayP = new Pixel[100];
-            Console.Write("Nhập số pixel cần xử lý: ");
+             Pixel[] arrayP = new Pixel[100];
+            Console.Write("Nhap so Pixel can xu ly: ");
             int n = int.Parse(Console.ReadLine());
             int count = 0;
-            Console.WriteLine("\t\t\n\nNHẬP PIXEL");
+            Console.WriteLine("Nhap Pixel");
             for (int i = 0; i < n; i++)
             {
                 arrayP[i] = new Pixel();
@@ -21,17 +21,15 @@ namespace daylamotproject
             {
                 arrayP[i].InPixcel();
             }
-            Console.ReadKey();
-            Console.Clear();
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("\t\nCác pixel trên đường chéo chính: ");
+            Console.WriteLine("Cac pixel tren duong cheo chinh: ");
             for (int i = 0; i < n; i++)
                 if (arrayP[i].KTCheoChinh())
                 {
                     arrayP[i].InPixcel();
                     count++;
                 }
-            Console.WriteLine("Số pixel trên đường chéo chính là: " + count + " điểm.");
+            Console.WriteLine("so pixcel tren duong cheo chinh la: " + count + " điểm.");
         }
     }
 }
