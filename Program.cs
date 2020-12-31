3a31de7815ed933746ad82e505d3c3779f0bf74d
Notepower <56997375+Notepower@users.noreply.github.com>
2020-12-31 12:59:02 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LAB_4
+namespace BAIKT
 {
     class Program
     {
@@ -12,77 +12,10 @@ namespace LAB_4
         {
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
-
-            //BAI 4.2
-
-            /*Student s = new Student();
-            s.Set();
-            s.Get();*/
-
-            //BAI 4.3
-
-            /*Pixel[] arrayP = new Pixel[100];
-            Console.Write("Nhập số pixel cần xử lý: ");
-            int n = int.Parse(Console.ReadLine());
-            int count = 0;
-            Console.WriteLine("\t\t\n\nNHẬP PIXEL");
-            for (int i = 0; i < n; i++)
-            {
-                arrayP[i] = new Pixel();
-                arrayP[i].Set();
-            }
-            Console.WriteLine("\t\t\n\nXUẤT PIXEL");
-            for (int i = 0; i < n; i++)
-            {
-                arrayP[i].InPixcel();
-            }
-            Console.WriteLine("\t\nCác pixel trên đường chéo chính: ");
-            for (int i = 0; i < n; i++)
-                if (arrayP[i].KTCheoChinh())
-                {
-                    arrayP[i].InPixcel();
-                    count++;
-                }
-            Console.WriteLine("Số pixel trên đường chéo chính là: " + count + " điểm.");*/
-
-            /* HanhKhach k1 = new HanhKhach();
-            k1.Set();
-            k1.Get();*/
-            HanhKhach[] k = new HanhKhach[10];
-            int n;
-            Console.Write("Nhập số hành khách xử lý: ");
-            n = int.Parse(Console.ReadLine());
-            Console.Write("------------------------------------------------------------------------------");
-            Console.WriteLine("\t\t\n\nNHẬP THÔNG TIN HÀNH KHÁCH");
-            for (int i = 0; i < n; i++)
-            {
-                k[i] = new HanhKhach();
-                Console.WriteLine("\t\nNHẬP HÀNH KHÁCH THỨ {0}", i + 1);
-                k[i].Set();
-            }
-            Console.Write("------------------------------------------------------------------------------");
-            Console.WriteLine("\t\t\n\nTHÔNG TIN HÀNH KHÁCH");
-            for (int i = 0; i < n; i++)
-            {
-                Console.WriteLine("\t\nHÀNH KHÁCH THỨ {0}", i + 1);
-                k[i].Get();
-            }
-            Console.Write("------------------------------------------------------------------------------");
-            for (int i = 0; i < n - 1; i++)
-                for (int j = 0; j < n - i - 1; j++)
-                    if (k[j].TongTien() > k[j + 1].TongTien())
-                    {
-                        HanhKhach temp = k[j];
-                        k[j] = k[j + 1];
-                        k[j + 1] = temp;
-                    }
-            Console.WriteLine("\t\t\n\nTHÔNG TIN HÀNH KHÁCH SAU KHI SẮP XẾP");
-            for (int i = 0; i < n; i++)
-            {
-                Console.WriteLine("\t\nHÀNH KHÁCH THỨ {0}", i + 1);
-                k[i].Get();
-            }
-            Console.Write("------------------------------------------------------------------------------");
+            nSquare s = new nSquare();
+            Console.WriteLine("\t\tDanh sách các HCN:");
+            s.Nhap();
+            s.Xoa();
             Console.ReadKey();
         }
     }
---
+++ Shape.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BAIKT
+{
+    abstract class Shape
+    {
+        string name;
+        public Shape()
+        {
+            name = "";
+        }
+        public Shape(string name)
+        {
+            this.name = name;
+        }
+        public Shape(Shape a)
+        {
+            name = a.name;
+        }
+        public abstract double CV();
+        public abstract double DT();
+        public void Output()
+        {
+            Console.Write("Name: {0} \t CV: {1} \t S: {2} \t", name, CV(), DT());
+        }
+    }
+    interface IPaint
+    {
+        float Cost(int c);
+    }
+    class Square : Shape,IPaint
+    {
+        float d;
+        float r;
+        public Square() : base()
+        {
+            d = 0;
+            r = 0;
+        }
+        public Square(string name, float d,float r) : base(name)
+        {
+            this.d = d;
+            this.r = r;
+        }
+
+        public Square(Square a)
+        {
+            d = a.d;
+            r = a.r;
+        }
+
+       
+
+        public override double CV()
+        {
+            return (d + r) * 2;
+        }
+        public override double DT()
+        {
+            return d * r;
+        }
+        public float Cost(int c)
+        {
+            return c * (float)DT();
+        }
+    }
+    class nSquare
+    {
+        int n, c;
+        List<Square> ds = new List<Square>();
+        public void Nhap()
+        {
+            FileStream f = new FileStream("Square.txt", FileMode.Open);
+            StreamReader rd = new StreamReader(f, Encoding.UTF8);
+            n = Convert.ToInt32(rd.ReadLine());
+            c = Convert.ToInt32(rd.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string name = rd.ReadLine();
+                float d = Convert.ToInt64(rd.ReadLine());
+                float r = Convert.ToInt64(rd.ReadLine());
+                Square a = new Square(name,d,r);
+                ds.Add(a);
+                f.Close();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                ds[i].Output();
+                Console.WriteLine("Price: " + ds[i].Cost(c));
+            }
+        }
+        public void Xoa()
+        {
+            List<Square> a = new List<Square>(ds);
+            float x = Convert.ToSingle(Console.ReadLine());
+            for (int i = 0; i <= n; i++)
+                if (a[i].DT() < x)
+                {
+                    a.RemoveAt(i);
+                    return;
+                }
+        }
+       
+    }
+}
+
