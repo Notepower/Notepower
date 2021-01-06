c11829f3e0a2d1de31b8ce3f6d3e68e59f4998f8
Notepower <56997375+Notepower@users.noreply.github.com>
2021-01-06 19:33:26 +0700

    Add files via upload

---
+++ ONKT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            nCreditCard n = new nCreditCard();
+            n.Input();
+            n.Paid(2, 2000);
+            //n.SX();
+            n.Output();
+            //n.Delete();
+            
+            Console.ReadKey();
+        }
+    }
+}
