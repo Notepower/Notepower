199ea95a0f6885fccee7a0db4ababda16f159f6c
Notepower <56997375+Notepower@users.noreply.github.com>
2021-01-06 19:31:13 +0700

    OOPP

--- Program.cs
+++ Program.cs
@@ -1,22 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BAIKT
+namespace ConsoleApp11
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.InputEncoding = Encoding.Unicode;
-            Console.OutputEncoding = Encoding.Unicode;
-            nSquare s = new nSquare();
-            Console.WriteLine("\t\tDanh sách các HCN:");
-            s.Nhap();
-            s.Xoa();
+            Console.WriteLine("DANH SACH TAI KHOAN");
+            nPersonalAccount n = new nPersonalAccount();
+            n.Input();
+            n.Output();
+            /*
+            Console.Write("Tai khoan can nap:");
+            byte t = byte.Parse(Console.ReadLine());
+            Console.Write("So Tien can nap:");
+            int m = int.Parse(Console.ReadLine());
+            n.Add(t,m);
+            */
+            n.SX();
+            Console.Write("NHAP TAI KHOAN CAN KIEM TRA:");
+            string stk = Console.ReadLine();
+            n.Check(stk);
             Console.ReadKey();
+
         }
     }
 }
